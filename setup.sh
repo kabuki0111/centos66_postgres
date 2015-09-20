07cfe874a199747f6c8ce50bd8d39732ce725d2d
Kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2015-09-20 10:59:48 +0900

    C＋＋が使えるように対応

--- setup.sh
+++ setup.sh
@@ -4,19 +4,26 @@ echo "### パッケージのインストール開始 ###"
 yum -y install httpd php postgresql postgresql-server php-mbstring php-pgsql php-gd
 
 echo "### Pythonに必要なパッケージをインストール開始 ###"
-yum -y install wget gcc zlib-devel openssl-devel readline-devel ncurses-devel sqlite-devel expat-devel bzip2-devel tcl-devel tk-devel gdbm-devel libbsd-devel
+yum -y install wget gcc gcc-c++ zlib-devel openssl-devel readline-devel ncurses-devel sqlite-devel expat-devel bzip2-devel tcl-devel tk-devel gdbm-devel libbsd-devel
+
+echo "### C++に必要なパッケージをインストール開始 ###"
+yum -y install devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++ devtoolset-2-gcc-gfortran
 
 echo "### Python3.3.3をダウンロード ###"
 wget http://www.python.org/ftp/python/3.3.3/Python-3.3.3.tgz
-tar zxvf Python-3.3.3.tgz
-
-echo "### Python3.3.3の設定開始 ###"
-cd  Python-3.3.3
-./configure --prefix=/usr/local/python-3.3
+mv -f Python-3.3.3.tgz /usr/local/src/
+tar zxvf /usr/local/src/Python-3.3.3.tgz
+mv -f Python-3.3.3 /usr/local/src/
+/usr/local/src/Python-3.3.3/configure --prefix=/usr/local/python-3.3
 make
 make install
 export PATH=/usr/local/python-3.3/bin:$PATH
 ln -s /usr/local/python-3.3/bin/python3.3 /usr/local/bin/python-test
+
+echo "### GCC4.8をインストール ###"
+cd  /etc/yum.repos.d/
+wget http://people.centos.org/tru/devtools-2/devtools-2.repo
+scl enable devtoolset-2 bash
 
 echo "### httpdを起動 ###"
 chkconfig httpd on
@@ -59,6 +66,15 @@ echo "?>"                                                                       
 echo "### Pythonにテスト用コード作成 ###"
 echo "print(\"Hello World\")"		> /var/www/html/check.py
 
+echo "### C++にテスト用コード作成 ###"
+echo "#include <iostream>"                      >       /var/www/html/check.cc
+echo "using namespace std;"                     >>      /var/www/html/check.cc
+echo ""                                         >>      /var/www/html/check.cc
+echo "int main(){"                              >>      /var/www/html/check.cc
+echo "cout << \"Hello World! C++\" << endl;"    >>      /var/www/html/check.cc
+echo "return 0;"                                >>      /var/www/html/check.cc
+echo "}"                                        >>      /var/www/html/check.cc
+
 echo ### パッケージを更新開始 ###
 yum -y update
 echo ### パッケージを更新終了 ###
@@ -69,3 +85,10 @@ php /var/www/html/check_db.php
 
 echo "### Python3.3の動作検証 ###"
 python3.3 /var/www/html/check.py
+
+echo "### C++の動作検証ファイルをコンパイル ###"
+cd/var/www/html
+g++ check.cc -o check_c++
+echo "### C++の動作検証ファイルを実行 ###"
+cd/var/www/html
+./check_c++
