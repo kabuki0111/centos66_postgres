e5b5671446a9b7d990c2fa698a3d2d501f736ef4
Kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2015-09-20 11:18:14 +0900

    修正

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@
 echo "### パッケージのインストール開始 ###"
 yum -y install httpd php postgresql postgresql-server php-mbstring php-pgsql php-gd
 
-echo "### Pythonに必要なパッケージをインストール開始 ###"
-yum -y install wget gcc gcc-c++ zlib-devel openssl-devel readline-devel ncurses-devel sqlite-devel expat-devel bzip2-devel tcl-devel tk-devel gdbm-devel libbsd-devel
-
 echo "### C++に必要なパッケージをインストール開始 ###"
-yum -y install devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++ devtoolset-2-gcc-gfortran
+yum -y install wget gcc gcc-c++ devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++ devtoolset-2-gcc-gfortran
+
+#echo "### Pythonに必要なパッケージをインストール開始 ###"
+#yum -y install zlib-devel openssl-devel readline-devel ncurses-devel sqlite-devel expat-devel bzip2-devel tcl-devel tk-devel gdbm-devel libbsd-devel
 
 #echo "### Python3.3.3をダウンロード ###"
 #wget http://www.python.org/ftp/python/3.3.3/Python-3.3.3.tgz
@@ -29,10 +29,8 @@ echo "### httpdを起動 ###"
 chkconfig httpd on
 service httpd start
 
-
 echo "### Postgresqlを初期化 ###"
 service postgresql initdb
-
 
 echo "### postgresql.confの書き換え開始 ###"
 sed -i -e "59s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /var/lib/pgsql/data/postgresql.conf
@@ -42,17 +40,14 @@ sed -i -e "334s/#log_line_prefix = ''/log_line_prefix = '%t %u %d'/"    /var/lib
 echo "### pg_hba.confの書き換え開始 ###"
 sed -i -e "70,74s/ident/trust/"	/var/lib/pgsql/data/pg_hba.conf
 
-
 echo "### postgresqlの起動開始 ###"
 chkconfig postgresql on
 service postgresql start
-
 
 echo "### Postgresqlの設定開始 ###"
 sudo -u postgres psql -c "alter user postgres with password 'test1192'"
 sudo -u postgres createuser -s apache
 sudo -u postgres createdb testdb
-
 
 echo "### ドキュメント・ルートにテスト用コード作成 ###"
 echo "<?php"                                                                    >       /var/www/html/check_db.php
@@ -78,7 +73,6 @@ echo "}"                                        >>      /var/www/html/check.cc
 echo ### パッケージを更新開始 ###
 yum -y update
 echo ### パッケージを更新終了 ###
-
 
 echo "### 接続テスト結果 ###"
 php /var/www/html/check_db.php
